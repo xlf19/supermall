76be4317b33e2b98a770a9901e5bc8c1c77b2a9f
xlf19 <2107595528@qq.com>
2020-10-28 14:53:16 +0800

    xlf

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//1.安装插件
+Vue.use(VueRouter)
+//2.创建vue-router
+const routers = []
+const router = new VueRouter({
+    routers
+})
+//3.导出router
+export default router
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//     configureWebpack: {
+//         resolve: {
+//             alise: {
+//                 'assets': '@assets',
+//                 'common': '@common',
+//                 'components': '@components',
+//                 'network': '@network',
+//                 'views': '@views',
+//             }
+//         }
+//     }
+// }
